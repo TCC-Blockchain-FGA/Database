b5a3634c9ce1ce85c1e51aa44fa755c67bff19c1
Nilo Mendonca <nilojrmendonca@gmail.com>
2023-05-14 14:12:20 -0300

    Dump file update

--- dump/01-init-db.sql
+++ dump/01-init-db.sql
@@ -1,8 +1,6 @@
-DROP TABLE IF EXISTS movies;
-CREATE TABLE movies (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR NOT NULL,
-    release_year SMALLINT,
-    genre genre,
-    price NUMERIC(4, 2)
+DROP TABLE IF EXISTS wallets;
+CREATE TABLE wallets (
+    id VARCHAR PRIMARY KEY,
+    key VARCHAR NOT NULL,
+    key VARCHAR NOT NULL,
 );

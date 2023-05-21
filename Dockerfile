f75b7073bb3283e36c47fb858ef996f6cc5c4a9f
Nilo Mendonca <nilojrmendonca@gmail.com>
2023-05-21 18:42:32 -0300

    Add new tables

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,1 @@
 FROM postgres:15.1-alpine
-
-COPY dump/*.sql /docker-entrypoint-initdb.d/
--- dump/01-init-db.sql
+++ dump/01-init-db.sql
@@ -1,6 +1,29 @@
 DROP TABLE IF EXISTS wallets;
 CREATE TABLE wallets (
-    id VARCHAR PRIMARY KEY,
-    key VARCHAR NOT NULL,
-    key VARCHAR NOT NULL,
+    id SERIAL PRIMARY KEY,
+    key VARCHAR NOT NULL
 );
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    login VARCHAR NOT NULL,
+    phone VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    gender VARCHAR NOT NULL,
+    dateOfBirth VARCHAR NOT NULL,
+    address VARCHAR NOT NULL,
+    maritalStatus VARCHAR NOT NULL,
+    multipleBirth VARCHAR NOT NULL,
+    contactRelationship VARCHAR NOT NULL,
+    contactName VARCHAR NOT NULL,
+    contactPhone VARCHAR NOT NULL,
+    contactAddress VARCHAR NOT NULL,
+    contactGender VARCHAR NOT NULL,
+    languages VARCHAR NOT NULL,
+    preferredLanguage VARCHAR,
+    generalPractitioner VARCHAR
+);
+
+INSERT INTO users(login, phone, name, password, gender, dateOfBirth, address, maritalStatus, multipleBirth, contactRelationship, contactName, contactPhone, contactAddress, contactGender, languages, preferredLanguage, generalPractitioner) VALUES('user01@gmail.com', '61999999999', 'Usuário 01', '$2b$12$POQ.KYV3KX/XegxUY.x3huU4ehJSngIjugegJHpaM.sXG4QAfT8yi', 'M', '2020-05-05', 'RUA xpto APTo 25', 'DIVORCED', 'N', 'friends', 'Usuário 2', '618888888888', 'RUA 46 APTo 60', 'M', 'português, inglês, espanhol', 'português', 'Doutor X');
